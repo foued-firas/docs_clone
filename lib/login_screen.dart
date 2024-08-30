41c24939aa612a27e061bd44b01ba48336bfd6bb
firas foued <firas.fouad@isimg.tn>
2024-08-30 16:11:20 +0100

    docs-clone

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,14 +1,24 @@
 import 'package:docs_clone/colors.dart';
+import 'package:docs_clone/repository/auth_repository.dart';
 import 'package:flutter/material.dart';
-class Login_screen extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+class Login_screen extends  ConsumerWidget {
   const Login_screen({super.key});
+  void signInWithGoogle(WidgetRef ref){
+    ref.read(
+      authRepositoryProvider
+    ).signInWithGoogle();
+  }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context , WidgetRef ref) {
+    ref.watch(
+      authRepositoryProvider
+    ).signInWithGoogle();
     return Scaffold(
       body: Center(
         child: ElevatedButton.icon(
-          onPressed: (){}, 
+          onPressed: ()=>  signInWithGoogle(ref), 
           icon:Image.asset('assets/g-logo-2.png', 
           height: 20,) ,
           label: Text('Sign in with Google',
